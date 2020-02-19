e647c7334c5cdd024cf133e98cf7af478c13c239
Wender da Silva Paula <wendersp@gmail.com>
2020-02-19 11:46:51 -0300

    Estruna inicial do Sistemas

---
+++ src/main/java/br/com/wpsistemas/exemploswing/ExemploSwing.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.wpsistemas.exemploswing;
+
+import br.com.wpsistemas.view.TelaPrincipal;
+
+/**
+ *
+ * @author wender
+ */
+public class ExemploSwing {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        new TelaPrincipal().setVisible(true);
+    }
+    
+}
